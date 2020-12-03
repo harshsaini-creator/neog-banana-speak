d677914d933136c9b2d8bb6788bad5f94d765df2
harshsaini-creator <60533528+harshsaini-creator@users.noreply.github.com>
2020-12-03 13:01:04 +0530

    taking input and console

---
+++ app.js
@@ -0,0 +1,14 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var txtinput = document.querySelector("#txt-input");
+
+btnTranslate.addEventListener("click",function clickEventHandler(){
+
+    console.log("Clicked");
+    console.log("input" , txtinput.value)
+    // var name = prompt("Your Name :");
+    // alert("Great You are " + name);
+    // open("https://punjab.chitkara.edu.in//Interface/Student/index.php")
+})
+
+
+
